dcabb3b748858932c56c7d26aef6ae63a2b267b7
Raisa Silea <rs1753@live.mdx.ac.uk>
2024-01-08 22:52:36 +0000

    librarian class header

---
+++ librarian.h
@@ -0,0 +1,35 @@
+#ifndef LIBRARIAN_H
+#define LIBRARIAN_H
+
+#include member.h
+#include <vector>
+#include <unordered_map>
+
+class Book; 
+
+extern std::vector<Member> members; 
+extern std::unordered_map<int, Book> books; 
+
+class Librarian : public Person {
+private:
+    int staffId;
+    int salary;
+
+public:
+    // Constructor
+    Librarian(int staffId, const std::string& name, const std::string& address, 
+              const std::string& email, int salary);
+
+    // Getters and setters
+    int getStaffId() const;
+    void setStaffId(int newStaffId);
+    int getSalary() const;
+    void setSalary(int newSalary);
+    void addMember();
+    void issueBook(int memberId, int bookId);
+    void returnBook(int memberId, int bookId);
+    void displayBorrowedBooks(int memberId);
+    void calcFines(int memberId);
+};
+
+#endif 
