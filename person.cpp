1f15c9d49f373355fa23845cb208df03aed58bdf
Raisa Silea <rs1753@live.mdx.ac.uk>
2024-01-11 11:26:05 +0000

    person class setters and getters - member function definitions

---
+++ person.cpp
@@ -0,0 +1,28 @@
+#include "person.h"
+
+Person::Person(const std::string& name, const std::string& address, const std::string& email)
+    : name(name), address(address), email(email) {}
+
+std::string Person::getName() const {
+    return name;
+}
+
+std::string Person::getAddress() const {
+    return address;
+}
+
+std::string Person::getEmail() const {
+    return email;
+}
+
+void Person::setName(const std::string& newName) {
+    name = newName;
+}
+
+void Person::setAddress(const std::string& newAddress) {
+    address = newAddress;
+}
+
+void Person::setEmail(const std::string& newEmail) {
+    email = newEmail;
+}
