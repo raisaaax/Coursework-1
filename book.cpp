b93f32c201c474531928554c5e4d69c5d2100e37
Raisa Silea <rs1753@live.mdx.ac.uk>
2024-01-12 00:14:39 +0000

    initial commit

---
+++ book.cpp
@@ -0,0 +1,48 @@
+#include "book.h"
+#include "member.h"
+
+Book::Book(int bookID, const std::string &bookName,
+           const std::string &authorFirstName, const std::string &authorLastName)
+    : bookID(bookID), bookName(bookName),
+      authorFirstName(authorFirstName),  borrower(&placeholder), authorLastName(authorLastName) {}
+
+int Book::getBookID() const
+{
+    return bookID;
+}
+
+std::string Book::getBookName() const
+{
+    return bookName;
+}
+
+std::string Book::getAuthorFirstName() const
+{
+    return authorFirstName;
+}
+
+std::string Book::getAuthorLastName() const
+{
+    return authorLastName;
+}
+
+void Book::returnBook()
+{
+    return null
+}
+
+void Book::borrowBook(Member member, const Date &date)
+{
+    dueDate = date;
+    borrower = &member;
+}
+
+void Book::setDueDate(const Date &newDueDate)
+{
+    dueDate = newDueDate;
+}
+
+Date Book::getDueDate() const
+{
+    return dueDate;
+}
