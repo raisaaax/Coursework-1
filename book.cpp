9bc8a9f4767f1bdd5d7fb8238f9ccdd641face49
Raisa Silea <rs1753@live.mdx.ac.uk>
2024-01-15 20:45:55 +0000

    working final commit

--- book.cpp
+++ book.cpp
@@ -1,6 +1,8 @@
 #include "book.h"
 #include "member.h"
 
+static Member placeholder = Member(0, "PLACEHOLDER", "PLACEHOLDER", "PLACEHOLDER" ) 
+
 Book::Book(int bookID, const std::string &bookName,
            const std::string &authorFirstName, const std::string &authorLastName)
     : bookID(bookID), bookName(bookName),
@@ -28,7 +30,9 @@ std::string Book::getAuthorLastName() const
 
 void Book::returnBook()
 {
-    return null
+  dueDate = Date();
+  borrower = &placeholder;
+  
 }
 
 void Book::borrowBook(Member member, const Date &date)
